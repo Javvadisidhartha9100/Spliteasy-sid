65b0a57b0016a4e4b145fbfa163a39bec81604d9
Sidhartha javvadi <javvadisidhartha9100@gmail.com>
2026-03-11 19:14:41 -0400

    Initial Commit

---
+++ Spliteasy/SpliteasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpliteasyApp.swift
+//  Spliteasy
+//
+//  Created by SIDHARTHA JAVVADI on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SpliteasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
